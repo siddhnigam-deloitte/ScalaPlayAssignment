76930dc00717cb94985d24637f2a9b5c65971431
siddhnigam-deloitte <101544049+siddhnigam-deloitte@users.noreply.github.com>
2023-05-05 00:34:18 +0530

    Add files via upload

---
+++ ScalaPlay/target/scala-2.13/routes/main/controllers/javascript/JavaScriptReverseRoutes.scala
@@ -0,0 +1,93 @@
+// @GENERATOR:play-routes-compiler
+// @SOURCE:conf/routes
+
+import play.api.routing.JavaScriptReverseRoute
+
+
+import _root_.controllers.Assets.Asset
+
+// @LINE:9
+package controllers.javascript {
+
+  // @LINE:9
+  class ReverseMatchController(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:9
+    def getAllmatches: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.MatchController.getAllmatches",
+      """
+        function() {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "getAllMatches"})
+        }
+      """
+    )
+  
+    // @LINE:10
+    def getmatch: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.MatchController.getmatch",
+      """
+        function(matchid0) {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "Match/" + encodeURIComponent((""" + implicitly[play.api.mvc.PathBindable[Int]].javascriptUnbind + """)("matchid", matchid0))})
+        }
+      """
+    )
+  
+  }
+
+  // @LINE:12
+  class ReverseTimesWinController(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:12
+    def getwin: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.TimesWinController.getwin",
+      """
+        function(team0) {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "win/" + encodeURIComponent((""" + implicitly[play.api.mvc.PathBindable[String]].javascriptUnbind + """)("team", team0))})
+        }
+      """
+    )
+  
+    // @LINE:13
+    def getAllWins: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.TimesWinController.getAllWins",
+      """
+        function() {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "getallwins"})
+        }
+      """
+    )
+  
+  }
+
+  // @LINE:16
+  class ReverseAssets(_prefix: => String) {
+
+    def _defaultPrefix: String = {
+      if (_prefix.endsWith("/")) "" else "/"
+    }
+
+  
+    // @LINE:16
+    def versioned: JavaScriptReverseRoute = JavaScriptReverseRoute(
+      "controllers.Assets.versioned",
+      """
+        function(file1) {
+          return _wA({method:"GET", url:"""" + _prefix + { _defaultPrefix } + """" + "assets/" + (""" + implicitly[play.api.mvc.PathBindable[Asset]].javascriptUnbind + """)("file", file1)})
+        }
+      """
+    )
+  
+  }
+
+
+}
---
+++ ScalaPlay/target/scala-2.13/routes/main/router/Routes.scala
@@ -0,0 +1,180 @@
+// @GENERATOR:play-routes-compiler
+// @SOURCE:conf/routes
+
+package router
+
+import play.core.routing._
+import play.core.routing.HandlerInvokerFactory._
+
+import play.api.mvc._
+
+import _root_.controllers.Assets.Asset
+
+class Routes(
+  override val errorHandler: play.api.http.HttpErrorHandler, 
+  // @LINE:9
+  MatchController_0: controllers.MatchController,
+  // @LINE:12
+  TimesWinController_1: controllers.TimesWinController,
+  // @LINE:16
+  Assets_2: controllers.Assets,
+  val prefix: String
+) extends GeneratedRouter {
+
+   @javax.inject.Inject()
+   def this(errorHandler: play.api.http.HttpErrorHandler,
+    // @LINE:9
+    MatchController_0: controllers.MatchController,
+    // @LINE:12
+    TimesWinController_1: controllers.TimesWinController,
+    // @LINE:16
+    Assets_2: controllers.Assets
+  ) = this(errorHandler, MatchController_0, TimesWinController_1, Assets_2, "/")
+
+  def withPrefix(addPrefix: String): Routes = {
+    val prefix = play.api.routing.Router.concatPrefix(addPrefix, this.prefix)
+    router.RoutesPrefix.setPrefix(prefix)
+    new Routes(errorHandler, MatchController_0, TimesWinController_1, Assets_2, prefix)
+  }
+
+  private[this] val defaultPrefix: String = {
+    if (this.prefix.endsWith("/")) "" else "/"
+  }
+
+  def documentation = List(
+    ("""GET""", this.prefix + (if(this.prefix.endsWith("/")) "" else "/") + """getAllMatches""", """controllers.MatchController.getAllmatches()"""),
+    ("""GET""", this.prefix + (if(this.prefix.endsWith("/")) "" else "/") + """Match/""" + "$" + """matchid<[^/]+>""", """controllers.MatchController.getmatch(matchid:Int)"""),
+    ("""GET""", this.prefix + (if(this.prefix.endsWith("/")) "" else "/") + """win/""" + "$" + """team<[^/]+>""", """controllers.TimesWinController.getwin(team:String)"""),
+    ("""GET""", this.prefix + (if(this.prefix.endsWith("/")) "" else "/") + """getallwins""", """controllers.TimesWinController.getAllWins()"""),
+    ("""GET""", this.prefix + (if(this.prefix.endsWith("/")) "" else "/") + """assets/""" + "$" + """file<.+>""", """controllers.Assets.versioned(path:String = "/public", file:Asset)"""),
+    Nil
+  ).foldLeft(List.empty[(String,String,String)]) { (s,e) => e.asInstanceOf[Any] match {
+    case r @ (_,_,_) => s :+ r.asInstanceOf[(String,String,String)]
+    case l => s ++ l.asInstanceOf[List[(String,String,String)]]
+  }}
+
+
+  // @LINE:9
+  private[this] lazy val controllers_MatchController_getAllmatches0_route = Route("GET",
+    PathPattern(List(StaticPart(this.prefix), StaticPart(this.defaultPrefix), StaticPart("getAllMatches")))
+  )
+  private[this] lazy val controllers_MatchController_getAllmatches0_invoker = createInvoker(
+    MatchController_0.getAllmatches(),
+    play.api.routing.HandlerDef(this.getClass.getClassLoader,
+      "router",
+      "controllers.MatchController",
+      "getAllmatches",
+      Nil,
+      "GET",
+      this.prefix + """getAllMatches""",
+      """""",
+      Seq()
+    )
+  )
+
+  // @LINE:10
+  private[this] lazy val controllers_MatchController_getmatch1_route = Route("GET",
+    PathPattern(List(StaticPart(this.prefix), StaticPart(this.defaultPrefix), StaticPart("Match/"), DynamicPart("matchid", """[^/]+""",true)))
+  )
+  private[this] lazy val controllers_MatchController_getmatch1_invoker = createInvoker(
+    MatchController_0.getmatch(fakeValue[Int]),
+    play.api.routing.HandlerDef(this.getClass.getClassLoader,
+      "router",
+      "controllers.MatchController",
+      "getmatch",
+      Seq(classOf[Int]),
+      "GET",
+      this.prefix + """Match/""" + "$" + """matchid<[^/]+>""",
+      """""",
+      Seq()
+    )
+  )
+
+  // @LINE:12
+  private[this] lazy val controllers_TimesWinController_getwin2_route = Route("GET",
+    PathPattern(List(StaticPart(this.prefix), StaticPart(this.defaultPrefix), StaticPart("win/"), DynamicPart("team", """[^/]+""",true)))
+  )
+  private[this] lazy val controllers_TimesWinController_getwin2_invoker = createInvoker(
+    TimesWinController_1.getwin(fakeValue[String]),
+    play.api.routing.HandlerDef(this.getClass.getClassLoader,
+      "router",
+      "controllers.TimesWinController",
+      "getwin",
+      Seq(classOf[String]),
+      "GET",
+      this.prefix + """win/""" + "$" + """team<[^/]+>""",
+      """""",
+      Seq()
+    )
+  )
+
+  // @LINE:13
+  private[this] lazy val controllers_TimesWinController_getAllWins3_route = Route("GET",
+    PathPattern(List(StaticPart(this.prefix), StaticPart(this.defaultPrefix), StaticPart("getallwins")))
+  )
+  private[this] lazy val controllers_TimesWinController_getAllWins3_invoker = createInvoker(
+    TimesWinController_1.getAllWins(),
+    play.api.routing.HandlerDef(this.getClass.getClassLoader,
+      "router",
+      "controllers.TimesWinController",
+      "getAllWins",
+      Nil,
+      "GET",
+      this.prefix + """getallwins""",
+      """""",
+      Seq()
+    )
+  )
+
+  // @LINE:16
+  private[this] lazy val controllers_Assets_versioned4_route = Route("GET",
+    PathPattern(List(StaticPart(this.prefix), StaticPart(this.defaultPrefix), StaticPart("assets/"), DynamicPart("file", """.+""",false)))
+  )
+  private[this] lazy val controllers_Assets_versioned4_invoker = createInvoker(
+    Assets_2.versioned(fakeValue[String], fakeValue[Asset]),
+    play.api.routing.HandlerDef(this.getClass.getClassLoader,
+      "router",
+      "controllers.Assets",
+      "versioned",
+      Seq(classOf[String], classOf[Asset]),
+      "GET",
+      this.prefix + """assets/""" + "$" + """file<.+>""",
+      """ Map static resources from the /public folder to the /assets URL path""",
+      Seq()
+    )
+  )
+
+
+  def routes: PartialFunction[RequestHeader, Handler] = {
+  
+    // @LINE:9
+    case controllers_MatchController_getAllmatches0_route(params@_) =>
+      call { 
+        controllers_MatchController_getAllmatches0_invoker.call(MatchController_0.getAllmatches())
+      }
+  
+    // @LINE:10
+    case controllers_MatchController_getmatch1_route(params@_) =>
+      call(params.fromPath[Int]("matchid", None)) { (matchid) =>
+        controllers_MatchController_getmatch1_invoker.call(MatchController_0.getmatch(matchid))
+      }
+  
+    // @LINE:12
+    case controllers_TimesWinController_getwin2_route(params@_) =>
+      call(params.fromPath[String]("team", None)) { (team) =>
+        controllers_TimesWinController_getwin2_invoker.call(TimesWinController_1.getwin(team))
+      }
+  
+    // @LINE:13
+    case controllers_TimesWinController_getAllWins3_route(params@_) =>
+      call { 
+        controllers_TimesWinController_getAllWins3_invoker.call(TimesWinController_1.getAllWins())
+      }
+  
+    // @LINE:16
+    case controllers_Assets_versioned4_route(params@_) =>
+      call(Param[String]("path", Right("/public")), params.fromPath[Asset]("file", None)) { (path, file) =>
+        controllers_Assets_versioned4_invoker.call(Assets_2.versioned(path, file))
+      }
+  }
+}
